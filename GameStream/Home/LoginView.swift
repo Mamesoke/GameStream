f857a66a7ede3f957cc0f721c9b2b8aa0ee07229
Mamesoke <mamesoke@gmail.com>
2022-08-14 22:59:11 +0200

    Included user email to Login Screen

--- GameStream/Home/LoginView.swift
+++ GameStream/Home/LoginView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var email = ""
     var body: some View {
-        Text("Hello, Login!")
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text("Correo electrónico").foregroundColor(Color("dark-cian"))
+                ZStack(alignment: .leading) {
+                    if email.isEmpty {
+                        Text(verbatim: "ejemplo@gmail.com")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                    TextField("", text: $email)
+                        .foregroundColor(Color(.white))
+                }
+                Divider().frame(height: 1).background(Color("dark-cian"))
+            }.padding(.horizontal, 77.0)
+        }
     }
 }
 
