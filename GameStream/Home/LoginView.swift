fa78f76ad5a009f270f9ca09416ba1ddbd01025d
Mamesoke <mamesoke@gmail.com>
2022-08-16 13:31:20 +0200

    Login view screen visual completed

--- GameStream/Home/LoginView.swift
+++ GameStream/Home/LoginView.swift
@@ -8,7 +8,13 @@
 import SwiftUI
 
 struct LoginView: View {
+    enum RRSS {
+        case facebook
+        case twitter
+    }
     @State var email = ""
+    @State var pwd = ""
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
@@ -23,8 +29,53 @@ struct LoginView: View {
                         .foregroundColor(Color(.white))
                 }
                 Divider().frame(height: 1).background(Color("dark-cian"))
+                Text("Contraseña").foregroundColor(Color("dark-cian"))
+                ZStack(alignment: .leading) {
+                    if pwd.isEmpty {
+                        Text(verbatim: "Escribe su contraseña")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                    SecureField("", text: $pwd)
+                        .foregroundColor(Color(.white))
+                }
+                Divider().frame(height: 1).background(Color("dark-cian"))
+                Text("¿Olvidaste tu contraseña?")
+                    .foregroundColor(Color("dark-cian"))
+                    .font(.footnote)
+                    .frame(width: 300, alignment: .trailing)
+                    .padding(.bottom, 52)
+                Button(action: login, label: {
+                    Text("INICIAR SESIÓN")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18))
+                        .overlay(RoundedRectangle(cornerRadius: 6).stroke(Color("dark-cian"), lineWidth: 1.0)).shadow(color: .white, radius: 3)
+                })
+                Text("Inicia sesión con tus redes sociales").foregroundColor(.white)
+                HStack {
+                    Button(action: {
+                        login(withRRSS: .facebook)
+                    }, label: {
+                        Text("Facebook").foregroundColor(.white)
+                    })
+                    Button(action: {
+                        login(withRRSS: .twitter)
+                    }, label: {
+                        Text("Twitter").foregroundColor(.white)
+                    })
+                }
             }.padding(.horizontal, 77.0)
         }
+    }
+    
+    func login() {
+        print("Login")
+    }
+    
+    func login(withRRSS rrss: RRSS) {
+        print(rrss)
     }
 }
 
